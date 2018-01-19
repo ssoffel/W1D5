6ab2bc78b1d900765cb98c5f29a82287610267ae
Scott and Lifazul <ssoffel@hotmail.com>
2018-01-19 16:09:27 -0500

    Passed the second set of specs

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -2,33 +2,39 @@
 
 class PolyTreeNode
   attr_reader :parent, :children, :value
-  
+
   def initialize(value)
     @parent = nil
     @children = []
     @value = value 
   end 
   
+  def remove_from_children(child)
+    @children.delete(child)
+  end 
+  
   def parent=(above_parents)
     if @parent == above_parents 
-      return 
-    end 
-     
-    
+    elsif above_parents == nil 
+      @parent.children.delete(self)
+      @parent = nil 
+    else 
       @parent = above_parents
       above_parents.children << self
+    end
+
   end 
 
 end
 
-n1 = PolyTreeNode.new("root1")
-n2 = PolyTreeNode.new("root2")
-n3 = PolyTreeNode.new("root3")
-
- 
- 
-p n3.parent = n2
-p n3.parent = n1
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
+# # 
+# 
+# 
+# p n3.parent = nil 
+# p n3.parent
 # # connect n3 to n1
 # n3.parent = n1
 # n3.parent
